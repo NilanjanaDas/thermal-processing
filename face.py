508f29a2b4c366491ebaeb52fc4097f6a7537cde
Aadhar <tyagi.aadhar@gmail.com>
2020-05-28 02:17:37 +0530

    minor changes

--- face.py
+++ face.py
@@ -1,25 +1,29 @@
 import numpy as np
 import cv2
 
-
+# import face and eye cascade xml files
 face_cascade = cv2.CascadeClassifier('face.xml')
 eye_cascade = cv2.CascadeClassifier ('eye.xml')
 
+# opeing the default camera
 cap = cv2.VideoCapture(0)
 
 while 1:
     ret, img = cap.read()
+    # convert to gray and find faces first
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
+    # detect eyes only insdie the faces
     for (x,y,w,h) in faces:
-        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        cv2.rectangle(img,(x,y),(x+w,y+h),(200,0,0),2)
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = img[y:y+h, x:x+w]
 
+        # apply eye cascade and print a bounding box
         eyes = eye_cascade.detectMultiScale(roi_gray)
         for (ex,ey,ew,eh) in eyes:
-            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,0,200),2)
 
     cv2.imshow('img',img)
     k = cv2.waitKey(30) & 0xff
